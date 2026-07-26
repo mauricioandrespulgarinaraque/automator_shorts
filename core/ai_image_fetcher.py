e1b645e1460ad4d42a2bd817967798cf2b635bde
mauricioandrespulgarinaraque <69659199+mauricioandrespulgarinaraque@users.noreply.github.com>
2026-07-25 22:26:27 -0500

    sin iconos

--- core/ai_image_fetcher.py
+++ core/ai_image_fetcher.py
@@ -4,7 +4,7 @@ from config import VIDEO_WIDTH, VIDEO_HEIGHT
 
 class AIImageFetcher:
     def descargar_imagen(self, prompt: str, salida: str):
-        print(f"🖼️ Descargando imagen IA vertical...")
+        print(f"Descargando imagen IA vertical...")
         prompt_codificado = urllib.parse.quote(prompt)
         url = f"https://image.pollinations.ai/prompt/{prompt_codificado}?width={VIDEO_WIDTH}&height={VIDEO_HEIGHT}&nologo=true&seed=101"
         
